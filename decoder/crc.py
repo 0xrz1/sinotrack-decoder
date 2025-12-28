0fd14d264a99e2205942b0c54018f3327483ff70
Brey Laude <laude822@gmail.com>
2025-12-28 15:43:57 +0800

    decoder crc-16 ccitt

---
+++ decoder/crc.py
@@ -0,0 +1,11 @@
+def crc16_ccitt(data: bytes, poly=0x1021, init=0xFFFF):
+    crc = init
+    for b in data:
+        crc ^= b << 8
+        for _ in range(8):
+            if crc & 0x8000:
+                crc = (crc << 1) ^ poly
+            else:
+                crc <<= 1
+            crc &= 0xFFFF
+    return crc
